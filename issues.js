a5857670f10b4a1c888d86da6739e8cf478d958e
purplepaste5 <57466423+purplepaste5@users.noreply.github.com>
2023-07-09 11:07:24 -0700

    Update issues.js
    added some information about how to use local storage to persist favorited stories on a page refresh.

--- issues.js
+++ issues.js
@@ -6,7 +6,12 @@
 // Allow logged in users to “favorite” and “un-favorite” a story. These stories should remain favorited when the page refreshes.
 // Allow logged in users to see a separate list of favorited stories.
 
-// ----->How can i manage local storage to keep users data on the page even after the page refreshed. You can check what i did on stories.js line 79 to 96  
+// ----->How can i manage local storage to keep users data on the page even after the page refreshed. You can check what i did on stories.js line 79 to 96
+
+// you are close on how you can store this information. First, our goal is to store favorited stories in localstorage so that we can utilize that information in the putStoriesOnPage() function
+// to figure out which stories have been favorited already. Therefore, when we click to favorite a story, we need to store some unique identifying information about that story in localStorage
+// then when we refresh the page, the putStoriesOnPage() function will be called and we will ask localStorage if there are any favorited stories. If there are, we need to toggle the favorited
+// class on that story, and also add it to the favorites tab. 
 
 
 // any corrections that can make my project more appropriated? 
